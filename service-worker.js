661843e42b498eada7a917d2a0e7097c40127662
PBananarr <79418608+PBananarr@users.noreply.github.com>
2025-09-05 13:50:35 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'abnahme-cache-test-v2.0.1.';
+const CACHE_NAME = 'abnahme-cache-test-v2.0.2.';
 const ASSETS = [
   './',
   './index.html',
@@ -67,4 +67,5 @@ self.addEventListener('fetch', (event) => {
     }
   })());
 });
+
 
